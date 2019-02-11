4b586b54bcf138c5e5ec158038f1088cd2342908
walrobledo <walgrobledo@gmail.com>
2019-02-11 11:07:14 -0300

    updated edit remove post

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
 class PostController extends AbstractController
@@ -59,6 +60,41 @@ class PostController extends AbstractController
     }
 
     /**
+     * @Route("/user/post/mylist/{page}",
+     *     name="post.mylist",
+     *     methods="GET",
+     *     defaults={"page": 1},
+     *     requirements={"page" = "\d+"}
+     * )
+     *
+     * @param EntityManagerInterface $em
+     * @param PaginatorInterface $paginator
+     * @param int $page
+     *
+     * @return Response
+     */
+    public function mylist(EntityManagerInterface $em, PaginatorInterface $paginator, TokenStorageInterface $tokenStorage, int $page) : Response
+    {
+        $user = $tokenStorage->getToken()->getUser();
+
+        $posts = $paginator->paginate(
+            $em->getRepository(Post::class)->createQueryBuilder('j')
+            ->andWhere('j.author = :val')
+            ->setParameter('val', $user),
+            $page,
+            $this->getParameter('number_items'),
+            [
+                PaginatorInterface::DEFAULT_SORT_FIELD_NAME => 'j.publishedAt',
+                PaginatorInterface::DEFAULT_SORT_DIRECTION => 'DESC',
+            ]
+        );
+
+        return $this->render('post/mylist.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    /**
      * Create post.
      *
      * @Route("/user/post/create", name="post.create", methods="GET|POST")
@@ -81,7 +117,7 @@ class PostController extends AbstractController
             $em->persist($post);
             $em->flush();
 
-            return $this->redirectToRoute('post.list');
+            return $this->redirectToRoute('post.mylist');
         }
 
         return $this->render('post/create.html.twig', [
@@ -122,5 +158,66 @@ class PostController extends AbstractController
             'post' => $post,
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * Edit post.
+     *
+     * @Route("/post/{id}/edit", name="post.edit", methods="GET|POST", requirements={"id" = "\d+"})
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param Post $post
+     *
+     * @return Response
+     */
+    public function edit(Request $request, EntityManagerInterface $em, Post $post, TokenStorageInterface $tokenStorage) : Response
+    {
+        $user = $tokenStorage->getToken()->getUser();
+
+        //controla que el usuario es el autor del post
+        if (! ($user === $post->getAuthor())) {
+            throw new AccessDeniedException('Unable to access this page!');
+        }
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('post.mylist');
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Delete post.
+     *
+     * @Route("/post/{id}/delete", name="post.delete", methods="DELETE", requirements={"id" = "\d+"})
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param Post $post
+     *
+     * @return Response
+     */
+    public function delete(Request $request, EntityManagerInterface $em, Post $post, TokenStorageInterface $tokenStorage) : Response
+    {
+        $user = $tokenStorage->getToken()->getUser();
+
+        if (! ($user === $post->getAuthor())) {
+            throw new AccessDeniedException('Unable to access this page!');
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $post->getId(), $request->request->get('_token'))) {
+            $em->remove($post);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('post.mylist');
     }
 }
